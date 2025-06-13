12ff6d1e57b41a5bc47fdaf8036d1dbcdf8e6f14
Nikhil <nikhil.sath99@gmail.com>
2025-06-13 18:34:04 +0100

    Remove unused default constants

--- db/bootstrap.py
+++ db/bootstrap.py
@@ -32,53 +32,6 @@ LAYOUT_DEFAULTS = {
     },
 }
 
-DEFAULT_CONFIGS = [
-    ("log_level", "INFO", "general", "string"),
-    ("handler_type", "rotating", "general", "string"),
-    ("max_file_size", "5242880", "general", "integer"),
-    ("backup_count", "3", "general", "integer"),
-    ("when_interval", "midnight", "general", "string"),
-    ("interval_count", "1", "general", "integer"),
-    (
-        "log_format",
-        "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-        "general",
-        "string",
-    ),
-    ("filename", "logs/crossbook.log", "general", "string"),
-    ("heading", "Load the glass cannons", "home", "string"),
-]
-
-DEFAULT_BASE_TABLE_ROWS = [
-    ("content", "Content", "Content Index", 1),
-    ("character", "Characters", "View all known characters in Alagaesia", 2),
-    ("thing", "Things", "Artifacts, tools, and curiosities", 3),
-    ("faction", "Factions", "Factions, cultures, and organizations", 4),
-    ("location", "Locations", "Important places throughout the land", 5),
-    (
-        "topic",
-        "Lore Topics",
-        "Themes, magic systems, and unanswered questions",
-        6,
-    ),
-]
-
-DEFAULT_FIELDS = {
-    "content": [
-        ("id", "hidden"),
-        ("content", "text"),
-        ("tags", "multi_select"),
-    ],
-    "character": [
-        ("id", "hidden"),
-        ("character", "text"),
-    ],
-    "thing": [("id", "hidden"), ("thing", "text")],
-    "faction": [("id", "hidden"), ("faction", "text")],
-    "location": [("id", "hidden"), ("location", "text")],
-    "topic": [("id", "hidden"), ("topic", "text")],
-}
-
 
 def _create_core_tables(cur: sqlite3.Cursor) -> None:
     cur.execute(
