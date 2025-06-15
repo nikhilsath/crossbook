f7257e5e0fdac66c6023ff5d607b7acc86701376
Nikhil <nikhil.sath99@gmail.com>
2025-06-15 18:22:19 +0100

    Move sidebar toggle to sidebar

--- static/js/sidebar_toggle.js
+++ static/js/sidebar_toggle.js
@@ -1,8 +1,14 @@
 const sidebar = document.getElementById('sidebar');
 const collapseBtn = document.getElementById('sidebarCollapse');
-collapseBtn.innerHTML = sidebar.classList.contains('hidden') ? '&raquo;' : '&laquo;';
+
+function setToggleIcon() {
+  collapseBtn.innerHTML = sidebar.classList.contains('md:block') ? '&laquo;' : '&raquo;';
+}
+
 function toggleSidebar() {
   sidebar.classList.toggle('md:block');
-  collapseBtn.innerHTML = sidebar.classList.contains('hidden') ? '&raquo;' : '&laquo;';
+  setToggleIcon();
 }
+
+setToggleIcon();
 collapseBtn.addEventListener('click', toggleSidebar);
