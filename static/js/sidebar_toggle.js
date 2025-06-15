82171281c20804a4de56c26f6a235b2fe477330f
Nikhil Sathyanarayana <nikhil.sath99@gmail.com>
2025-06-15 18:13:17 +0100

    fix sidebar toggle

--- static/js/sidebar_toggle.js
+++ static/js/sidebar_toggle.js
@@ -2,7 +2,7 @@ const sidebar = document.getElementById('sidebar');
 const collapseBtn = document.getElementById('sidebarCollapse');
 collapseBtn.innerHTML = sidebar.classList.contains('hidden') ? '&raquo;' : '&laquo;';
 function toggleSidebar() {
-  sidebar.classList.toggle('hidden');
+  sidebar.classList.toggle('md:block');
   collapseBtn.innerHTML = sidebar.classList.contains('hidden') ? '&raquo;' : '&laquo;';
 }
 collapseBtn.addEventListener('click', toggleSidebar);
