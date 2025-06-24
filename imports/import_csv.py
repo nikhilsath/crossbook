212decbd1d04f1077e11249600158f1fddb151ec
Nikhil <nikhil.sath99@gmail.com>
2025-06-24 15:03:15 +0100

    Ensure csv field size limit restored

--- imports/import_csv.py
+++ imports/import_csv.py
@@ -5,8 +5,12 @@ from io import StringIO
 def parse_csv(file):
     decoded = file.read().decode("utf-8")
     csv_stream = StringIO(decoded)
+    original_limit = csv.field_size_limit()
     csv.field_size_limit(sys.maxsize)
-    reader = csv.DictReader(csv_stream)
-    rows = list(reader)  
-    headers = reader.fieldnames
+    try:
+        reader = csv.DictReader(csv_stream)
+        rows = list(reader)
+        headers = reader.fieldnames
+    finally:
+        csv.field_size_limit(original_limit)
     return headers, rows
